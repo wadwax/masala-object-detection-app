05da26da68f3a28b77f1ad5e73c8466bb7c40650
wadwax <22623048+wadwax@users.noreply.github.com>
2022-08-29 12:21:25 +0900

    created request GUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import CredentialForm from "../components/credentials.svelte";
+  import Upload from "../components/upload.svelte";
+
+  let awsUsername, awsAccessKey, awsSecretKey, videoFile;
+
+  const handleRequest = async () => {
+    // request logic goes here
+  };
+</script>
+
+<div class="container">
+  <h1>Welcome to Masala</h1>
+  <CredentialForm bind:awsUsername bind:awsAccessKey bind:awsSecretKey />
+  <Upload bind:videoFile />
+  <button class="btn btn-main" on:click={handleRequest}>Request</button>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .btn {
+    width: 28rem;
+    padding: 0.8rem;
+    font-size: 16pt;
+    border-radius: 0.5rem;
+    border: none;
+    box-shadow: #ccc 0.1rem 0.2rem 0.1rem;
+  }
+  .btn-main {
+    background-color: rgb(45 212 191);
+    color: white;
+    cursor: pointer;
+  }
+</style>
